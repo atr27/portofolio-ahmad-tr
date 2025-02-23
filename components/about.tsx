16e094029fb9f779b930713b39008bcf4b876527
atr27 <2701ator@gmail.com>
2025-02-24 06:53:31 +0700

    feat: edit about

--- components/about.tsx
+++ components/about.tsx
@@ -19,7 +19,7 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        Setelah lulus dengan gelar sarjana, saya memutuskan untuk mengejar minat dan kemampuan saya di bidang pemprograman. 
+        Setelah menyelesaikan kuliah dan lulus dengan gelar sarjana S1, saya memutuskan untuk mengejar minat dan kemampuan saya di bidang pemprograman. 
         Saya mendaftar banyak kelas training coding dan fokus pengembangan skills pemrograman di{" "}
         <span className="font-medium">Full Stack Website Development</span>.{" "}
         <span className="font-medium">Bagian favorit saya dari pemprograman adalah</span> aspek pengembangan di bagian frontend dan backend. Saya suka
